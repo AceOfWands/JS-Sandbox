1cee7cfdecaeb3929aaf5a374679d795fcb83168
AndX <andrea0postiglione@gmail.com>
2020-05-27 22:09:20 +0200

    added externals for node execution

--- webpack.test.js
+++ webpack.test.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
 	mode: 'development',
@@ -8,6 +9,7 @@ module.exports = {
 	target: 'node',
 	output: {
 		filename: '[name].js',
-		path: path.resolve(__dirname, 'test', 'temp')
-	}
+		path: path.resolve(__dirname, 'test', 'build')
+	},
+	externals: [nodeExternals()]
 };
